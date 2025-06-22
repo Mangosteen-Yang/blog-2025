488dae4458ceb437d3937610e93c065940383caa
shuo <yangshuo225@gmail.com>
2025-06-22 22:09:54 +1000

    random image 1-4

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -4,7 +4,7 @@
  */
 export function getRandomCoverImage(): string {
   // We have 5 placeholder images (blog-placeholder-1.jpg through blog-placeholder-5.jpg)
-  const placeholderCount = 5;
+  const placeholderCount = 4;
   const randomIndex = Math.floor(Math.random() * placeholderCount) + 1;
   return `/images/blog-placeholder-${randomIndex}.jpg`;
 }
